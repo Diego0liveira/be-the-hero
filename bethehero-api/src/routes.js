804091ed8b8dd0f5d12ea0d356ea130e6d1527de
DiegoLOliveira <diegoluiz46@gmail.com>
2020-03-31 01:48:34 -0300

    Construção da api

---
+++ bethehero-api/src/routes.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const OngController =  require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
+
+const routes = express.Router()
+
+routes.get('/', (request, response) => {
+    return response.json({ label: 'api run ...' })
+})
+
+routes.post('/sessions', SessionController.create)
+
+routes.post('/ongs', OngController.create)
+
+routes.get('/ongs', OngController.index)
+
+routes.post('/incidents', IncidentController.create)
+
+routes.get('/incidents', IncidentController.index)
+
+routes.delete('/incidents/:id', IncidentController.delete)
+
+routes.get('/profile', ProfileController.index)
+
+module.exports = routes
